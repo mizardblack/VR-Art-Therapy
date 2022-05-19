bd19e25f4fa1a99d808cf0fd3a49f2113a29d073
Simone Sun <sunx660@newschool.edu>
2022-05-19 00:30:08 -0400

    Update tutorial UI

--- Assets/Scripts/ActionManager.cs
+++ Assets/Scripts/ActionManager.cs
@@ -35,7 +35,7 @@ public class ActionManager : MonoBehaviour
     public void ToPhase1() //when the target is the "tray"
     {
         float dist = Vector3.Distance(currentPosition, previousPosition);
-        if (dist > 0.01f) // check move distance
+        if (dist > 0.1f) // check move distance
         {
             phase = 1;
             levels[phase - 1].SetActive(false);
@@ -43,37 +43,48 @@ public class ActionManager : MonoBehaviour
         }
     }
 
-    public void ToPhase2() //when the target is the "toggle button"
+    public void ToPhase2() //when the target is the "tray"
     {
-        phase = 2;
+        float dist = Vector3.Distance(currentPosition, previousPosition);
+        if (dist > 0.01f) // check move distance
+        {
+            phase = 2;
+            levels[phase - 1].SetActive(false);
+            levels[phase].SetActive(true);
+        }
+    }
+
+    public void ToPhase3() //when the target is the "toggle button"
+    {
+        phase = 3;
         levels[phase - 1].SetActive(false);
         levels[phase].SetActive(true);
     }
 
-    public void ToPhase3() //when the target is the "slider"
+    public void ToPhase4() //when the target is the "slider"
     {
         float distX = Mathf.Abs(currentPosition.x - previousPosition.x);
         float distZ = Mathf.Abs(currentPosition.z - previousPosition.z);
 
         if (distX > 0.01f || distZ == 0f) // check move distance only on x axis
         {
-            phase = 3;
-            levels[phase - 1].SetActive(false);
-            levels[phase].SetActive(true);
-        }
-    }
-    public void ToPhase4() //when the target is the "grabber"
-    {
-        float distX = Mathf.Abs(currentPosition.x - previousPosition.x);
-        float distY = Mathf.Abs(currentPosition.y - previousPosition.y);
-        if (distX > 0.01f || distY == 0f) // check move distance only on x axis
-        {
             phase = 4;
             levels[phase - 1].SetActive(false);
             levels[phase].SetActive(true);
         }
     }
-    public void ToPhase5() //when the target is the "round button"
+    public void ToPhase5() //when the target is the "grabber"
+    {
+        float distX = Mathf.Abs(currentPosition.x - previousPosition.x);
+        float distY = Mathf.Abs(currentPosition.y - previousPosition.y);
+        if (distX > 0.01f || distY == 0f) // check move distance only on x axis
+        {
+            phase = 5;
+            levels[phase - 1].SetActive(false);
+            levels[phase].SetActive(true);
+        }
+    }
+    public void ToPhase6() //when the target is the "round button"
     {
         // phase = 5;
         Debug.Log("switch scene");
