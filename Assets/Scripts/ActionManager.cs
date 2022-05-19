ef9dec140cce6249930d2233f79ef180155b3ada
Simone Sun <sunx660@newschool.edu>
2022-05-19 15:49:23 -0400

    Fix run time error when UI diasppear

--- Assets/Scripts/ActionManager.cs
+++ Assets/Scripts/ActionManager.cs
@@ -29,17 +29,23 @@ public class ActionManager : MonoBehaviour
 
     public void RecordPreviousPos()
     {
-        previousPosition = targets[phase].transform.position;
+        if (phase < targets.Count)
+        {
+            previousPosition = targets[phase].transform.position;
+        }
     }
     public void RecordCurrentPos()
     {
-        currentPosition = targets[phase].transform.position;
+        if (phase < targets.Count)
+        {
+            currentPosition = targets[phase].transform.position;
+        }
     }
 
     public void ToPhase1() // when the target is the "brush"
     {
         float dist = Vector3.Distance(currentPosition, previousPosition);
-        if (dist > 0.1f) // check move distance
+        if (dist > 0.1f && phase == 0) // check move distance
         {
             phase = 1;
             // show next tutorial level
@@ -51,7 +57,7 @@ public class ActionManager : MonoBehaviour
     public void ToPhase2() // when the target is the "tray"
     {
         float dist = Vector3.Distance(currentPosition, previousPosition);
-        if (dist > 0.01f) // check move distance
+        if (dist > 0.01f && phase == 1) // check move distance
         {
             phase = 2;
             // show next tutorial level
@@ -62,10 +68,14 @@ public class ActionManager : MonoBehaviour
 
     public void ToPhase3() // when the target is the "toggle button"
     {
-        phase = 3;
-        // show next tutorial level
-        levels[phase - 1].SetActive(false);
-        levels[phase].SetActive(true);
+        if (phase == 2)
+        {
+            phase = 3;
+            // show next tutorial level
+            levels[phase - 1].SetActive(false);
+            levels[phase].SetActive(true);
+        }
+
     }
 
     public void ToPhase4() // when the target is the "slider"
@@ -75,10 +85,13 @@ public class ActionManager : MonoBehaviour
 
         if (distX > 0.01f || distZ == 0f) // check move distance only on x axis
         {
-            phase = 4;
-            // show next tutorial level
-            levels[phase - 1].SetActive(false);
-            levels[phase].SetActive(true);
+            if (phase == 3)
+            {
+                phase = 4;
+                // show next tutorial level
+                levels[phase - 1].SetActive(false);
+                levels[phase].SetActive(true);
+            }
         }
     }
     public void ToPhase5() // when the target is the "grabber"
@@ -87,58 +100,66 @@ public class ActionManager : MonoBehaviour
         float distY = Mathf.Abs(currentPosition.y - previousPosition.y);
         if (distX > 0.01f || distY == 0f) // check move distance only on x axis
         {
-            phase = 5;
-            // show next tutorial level
-            levels[phase - 1].SetActive(false);
-            levels[phase].SetActive(true);
+            if (phase == 4)
+            {
+
+                phase = 5;
+                // show next tutorial level
+                levels[phase - 1].SetActive(false);
+                levels[phase].SetActive(true);
+            }
         }
     }
     public void ToPhase6() // when the target is the "round button"
     {
-        Debug.Log("start");
-
-        // set "handles" position and scale
-        GameObject handle = GameObject.Find("Handle (1)");
-        handle.transform.localPosition = new Vector3(-0.39f, handle.transform.localPosition.y, handle.transform.localPosition.z);
-        handle = GameObject.Find("Handle (2)");
-        handle.transform.localPosition = new Vector3(0.369f, handle.transform.localPosition.y, handle.transform.localPosition.z);
-        handle.transform.localScale = new Vector3(4f, 1.5f, 4f);
-        // set "tray" scale
-        GameObject tray = GameObject.Find("Tray");
-        tray.transform.localScale = new Vector3(0.8f, 0.3f, 0.01f);
-
-        ShowAllUI();
-
-        // set "toggle button" position and scale
-        GameObject button = GameObject.Find("ToggleButton");
-        Debug.Log(button);
-        button.transform.localPosition = new Vector3(-0.209000006f, -0.326f, 0.136000007f);
-        button.transform.localScale = new Vector3(3f, 3f, 3f);
-        // set "slider" position and scale
-        GameObject slider = GameObject.Find("PinchSliderColorHue");
-        slider.transform.localPosition = new Vector3(-0.0850000009f, -0.283f, 0.1189995f);
-        slider.transform.localScale = new Vector3(1f, 1f, 1f);
-        // set "joystick" position and scale
-        GameObject joystick = GameObject.Find("JoystickPrefab (Scale)");
-        joystick.transform.localPosition = new Vector3(0.0860000029f, 0.256195843f, 0.0294604301f);
-        joystick.transform.localScale = new Vector3(0.030866839f, 0.030866839f, 0.030866839f);
-        joystick = GameObject.Find("JoystickContainer");
-        joystick.transform.localPosition = new Vector3(0.21570003f, -0.291000009f, 0.0350000001f);
-        GameObject joystickBase = GameObject.Find("Base (Scale)");
-        joystickBase.transform.localPosition = new Vector3(0f, -2.20000005f, -1.76999998f);
-        joystickBase.transform.localScale = new Vector3(3.52501631f, 0.962074459f, 7f);
-        // set "round button" position and scale
-        button = GameObject.Find("ClearCanvasButton");
-        button.transform.localPosition = new Vector3(0.383f, -0.317f, 0.128999993f);
-        button.transform.localScale = new Vector3(2f, 2f, 2f);
-
-        GameObject[] demos = GameObject.FindGameObjectsWithTag("Demo");
-        foreach (GameObject demo in demos)
+        if (phase == 5)
         {
-            demo.SetActive(false);
+            phase = 6;
+
+            // set "handles" position and scale
+            GameObject handle = GameObject.Find("Handle (1)");
+            handle.transform.localPosition = new Vector3(-0.39f, handle.transform.localPosition.y, handle.transform.localPosition.z);
+            handle = GameObject.Find("Handle (2)");
+            handle.transform.localPosition = new Vector3(0.369f, handle.transform.localPosition.y, handle.transform.localPosition.z);
+            handle.transform.localScale = new Vector3(4f, 1.5f, 4f);
+            // set "tray" scale
+            GameObject tray = GameObject.Find("Tray");
+            tray.transform.localScale = new Vector3(0.8f, 0.3f, 0.01f);
+
+            ShowAllUI();
+
+            // set "toggle button" position and scale
+            GameObject button = GameObject.Find("ToggleButton");
+            Debug.Log(button);
+            button.transform.localPosition = new Vector3(-0.209000006f, -0.326f, 0.136000007f);
+            button.transform.localScale = new Vector3(3f, 3f, 3f);
+            // set "slider" position and scale
+            GameObject slider = GameObject.Find("PinchSliderColorHue");
+            slider.transform.localPosition = new Vector3(-0.0850000009f, -0.283f, 0.1189995f);
+            slider.transform.localScale = new Vector3(1f, 1f, 1f);
+            // set "joystick" position and scale
+            GameObject joystick = GameObject.Find("JoystickPrefab (Scale)");
+            joystick.transform.localPosition = new Vector3(0.0860000029f, 0.256195843f, 0.0294604301f);
+            joystick.transform.localScale = new Vector3(0.030866839f, 0.030866839f, 0.030866839f);
+            joystick = GameObject.Find("JoystickContainer");
+            joystick.transform.localPosition = new Vector3(0.21570003f, -0.291000009f, 0.0350000001f);
+            GameObject joystickBase = GameObject.Find("Base (Scale)");
+            joystickBase.transform.localPosition = new Vector3(0f, -2.20000005f, -1.76999998f);
+            joystickBase.transform.localScale = new Vector3(3.52501631f, 0.962074459f, 7f);
+            // set "round button" position and scale
+            button = GameObject.Find("ClearCanvasButton");
+            button.transform.localPosition = new Vector3(0.383f, -0.317f, 0.128999993f);
+            button.transform.localScale = new Vector3(2f, 2f, 2f);
+
+            GameObject[] demos = GameObject.FindGameObjectsWithTag("Demo");
+            foreach (GameObject demo in demos)
+            {
+                demo.SetActive(false);
+            }
+
+            HideAllDemo();
         }
 
-        HideAllDemo();
     }
 
     private void ShowAllUI()
